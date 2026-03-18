cf0471df0ce72aa1b9645254bb6e3d5ac7a3ace3
omarmolina-dev <omar03molina@gmail.com>
2026-03-17 20:57:18 -0700

    add formspree contact form

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("#btn-sal");
-
-button.addEventListener("click", function() {
-    const titulo = document.querySelector("h1");
-    titulo.style.color = "#E8650A";
-    titulo.textContent = "You clicked me!";
-    
-});
-
